770091458e1ecfe19a998a3aa76e86d2e2596756
LayMorja <65667728+LayMorja@users.noreply.github.com>
2021-09-21 23:05:35 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+   $('.slider').slick({
+      autoplaySpeed: 2800,
+      arrows: false,
+      dots: true,
+      vertical: true,
+      verticalSwiping: true,
+      pauseOnHover: false,
+      slidesToShow: 1,
+   });
+ });
