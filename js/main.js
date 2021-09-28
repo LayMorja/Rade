b1451b72b805aedf30b465fa67ed1d0eb116db7a
LayMorja <evil-corporaty@yandex.ru>
2021-09-28 13:19:37 +0300

    Добавил стрелки, изменил точки, поправил меню в SASS

--- js/main.js
+++ js/main.js
@@ -1,15 +1,12 @@
 const iconMenu = document.querySelector('.icon-menu');
 const menuBody = document.querySelector('.menu__body');
 const body = document.querySelector('body');
-const menuItem = document.querySelector('.menu__item');
-const menuList = document.querySelector('.menu__list');
 
 $(document).ready(function(){
    $('.slider').slick({
-      autoplay: true,
-      arrows: false,
+      // autoplay: true,
+      // arrows: false,
       dots: true,
-      vertical: true,
       verticalSwiping: false,
       pauseOnHover: false,
       slidesToShow: 1,
@@ -22,8 +19,4 @@ iconMenu.addEventListener('click', function() {
    iconMenu.classList.toggle('active');
    menuBody.classList.toggle('active');
    body.classList.toggle('lock');
-})
-
-menuItem.addEventListener('click', function() {
-   menuList.classList.toggle('active');
 })
