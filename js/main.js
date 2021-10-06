bd7b579bfb7820f33451669dcfb8bd46664eeb07
LayMorja <evil-corporaty@yandex.ru>
2021-10-07 02:01:16 +0300

    Обновил меню, добавил новый раздел

--- js/main.js
+++ js/main.js
@@ -1,6 +1,33 @@
 const iconMenu = document.querySelector('.icon-menu');
 const menuBody = document.querySelector('.menu__body');
 const body = document.querySelector('body');
+const arrow = document.querySelectorAll('.arrow');
+
+let isMobile = {
+	Android: function() {return navigator.userAgent.match(/Android/i);},
+	BlackBerry: function() {return navigator.userAgent.match(/BlackBerry/i);},
+	iOS: function() {return navigator.userAgent.match(/iPhone|iPad|iPod/i);},
+	Opera: function() {return navigator.userAgent.match(/Opera Mini/i);},
+	Windows: function() {return navigator.userAgent.match(/IEMobile/i);},
+	any: function() {return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows());}
+};
+
+if(isMobile.any()){
+		body.classList.add('touch');
+	for(i = 0; i < arrow.length; i++){
+			let thisLink = arrow[i].previousElementSibling;
+			let subMenu = arrow[i].nextElementSibling;
+			let thisArrow = arrow[i];
+			thisLink.classList.add('parent');
+
+		arrow[i].addEventListener('click', function(){
+			subMenu.classList.toggle('open');
+			thisArrow.classList.toggle('active');
+		});
+	}
+}else{
+	body.classList.add('mouse');
+}
 
 $(document).ready(function(){
    $('.slider').slick({
